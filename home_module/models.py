158f1842b94f79f82ea2af245afc705a0d79bddd
studyonlyonline <studyonlyonline@gmail.com>
2018-09-09 11:37:46 +0530

    added table Schemes in the db

--- home_module/models.py
+++ home_module/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Schemes(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    terms_and_conditions = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
