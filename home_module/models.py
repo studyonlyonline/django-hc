315095d374fd642652585e6a7389f3ee0181da8e
studyonlyonline <studyonlyonline@gmail.com>
2018-09-09 11:42:46 +0530

    added image_url field in Schemes table in the db

--- home_module/models.py
+++ home_module/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Schemes(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+    image_url = models.URLField(default=None)
     title = models.CharField(max_length=255)
     terms_and_conditions = models.TextField()
     start_date = models.DateField()
